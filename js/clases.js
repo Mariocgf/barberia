7e94087c6a7bdea36ff3194b2d5357631d16d691
AlanLangelan <alanlangelan.2016@gmail.com>
2025-07-21 09:58:20 -0300

    ¨clases¨

---
+++ js/clases.js
@@ -0,0 +1,46 @@
+// Clase para representar una calificación
+class Calificacion {
+    constructor(nombre, descripcion, puntaje) {
+      this.nombre = nombre;           // quien califica
+      this.descripcion = descripcion; // texto del comentario
+      this.puntaje = puntaje;         // número (por ejemplo, 1–5)
+    }
+  }
+  
+  // Clase para representar un servicio
+  class Servicio {
+    constructor(nombre, precio, duracion) {
+      this.nombre = nombre;   // ej. "Corte de pelo"
+      this.precio = precio;   // en la moneda que uses
+      this.duracion = duracion; // en minutos
+    }
+  }
+  
+  // Clase para representar un barbero
+  class Barbero {
+    constructor(nombre,descripcion) {
+      this.nombre = nombre;
+      this.descripcion = descripcion;
+      this.calificaciones = []; // aquí guardamos instancias de Calificacion
+    }
+  }
+    
+  
+    
+  // Clase para representar la agenda general
+  class Agenda {
+    
+    constructor(fecha, servicio, nombreCliente, telefonoCliente, emailCliente, barbero = null) {
+      this.fecha = typeof fecha === 'string' ? new Date(fecha) : fecha;
+      this.servicio = servicio;
+      this.barbero = barbero; // puede quedar null
+      this.cliente = {
+        nombre: nombreCliente,
+        telefono: telefonoCliente,
+        email: emailCliente
+      };
+    }
+}
+  
+  
+    
