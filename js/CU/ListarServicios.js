67da3f89d1fc7b7b580595836a290fdda5296c5a
AlanLangelan <alanlangelan.2016@gmail.com>
2025-07-23 10:01:02 -0300

    ListarServicios

---
+++ js/CU/ListarServicios.js
@@ -0,0 +1,26 @@
+function listarServiciosUseCase(containerSelector, serviciosArray) {
+    const container = document.querySelector(containerSelector);
+    container.innerHTML = '';
+  
+    serviciosArray.forEach((s, idx) => {
+      const card = document.createElement('div');
+      card.className = 'card blur p-0';
+      card.style.width = '18rem';
+  
+      const img = document.createElement('img');
+      img.src = `img/servicio_${idx + 1}.webp`;
+      img.className = 'card-img-top img-fluid';
+      img.alt = s.nombre;
+      card.appendChild(img);
+  
+      const body = document.createElement('div');
+      body.className = 'card-body';
+      body.innerHTML = `
+        <h5 class="card-title">${s.nombre}</h5>
+        <h6 class="card-subtitle mb-2 text-body-secondary">Precio: $${s.precio}</h6>
+      `;
+      card.appendChild(body);
+  
+      container.appendChild(card);
+    });
+  }
--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", inicio);
 function inicio() {
+    listarServiciosUseCase("#servicios .row", servicios);
     document.getElementById("btn_calificar").addEventListener("click", calificarBarbero);
     poblarSelect(INPUT_BARBERO_CALIFICACION, "Seleccione un barbero", "BARBEROS");
     poblarSelect(filter-barbero, "Seleccione un barbero", "BARBEROS");
