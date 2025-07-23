c9160ce8fce155431ff88cf79426bb35c938802d
AlanLangelan <alanlangelan.2016@gmail.com>
2025-07-23 09:01:47 -0300

    ¨arreglos¨

--- js/index.js
+++ js/index.js
@@ -2,5 +2,5 @@ window.addEventListener("load", inicio);
 function inicio() {
     document.getElementById("btn_calificar").addEventListener("click", calificarBarbero);
     poblarSelect(INPUT_BARBERO_CALIFICACION, "Seleccione un barbero", "BARBEROS");
-    poblarSelect(INPUT_BARBERO_CALIFICACION, "Seleccione un barbero", "BARBEROS");
+    poblarSelect(filter-barbero, "Seleccione un barbero", "BARBEROS");
 }
--- js/variables.js
+++ js/variables.js
@@ -5,3 +5,8 @@ const INPUT_BARBERO_CALIFICACION = document.querySelector('#inputCalificacionBar
 const INPUT_RESENA_CALIFICAION = document.querySelector("#inputCalificacionResena");
 
 const INPUT_SERVICIO_AGENDA = document.querySelector('servicio-select');
+
+const loader = document.getElementById('loader');
+    const content = document.getElementById('reservas-content');
+    const tbody = document.getElementById('tbody-reservas');
+    const btnOrden = document.getElementById('sort-fecha-asc');
