375a48dda6a30cd414ea71b72046026153f11810
FACULTADES\alumnoFI <a>
2025-07-30 10:03:25 -0300

    logicaAgendar

--- js/CU/Agendar.js
+++ js/CU/Agendar.js
@@ -20,16 +20,12 @@
         return;
       }
 
-      console.log(nombre.value, email.value, servicio.value, slcBarberosAgenda.value, fecha.value, hora.value)
-
-      console.log(fecha.value)
-
       const nuevaFecha = new Date(fecha.value)
 
       
       const horaTotal = hora.value.split(":")
       
-      console.log(horaTotal)
+      console.log(agendas)
       
       nuevaFecha.setHours(horaTotal[0])
       nuevaFecha.setMinutes(horaTotal[1])
@@ -37,15 +33,150 @@
     
     const existe = agendas.some(cita => {
 
-        return (cita.fecha.getFullYear() == nuevaFecha.getFullYear() && cita.fecha.getMonth() == nuevaFecha.getMonth() && cita.fecha.getDate() == nuevaFecha.getDate() && cita.fecha.getHours() == nuevaFecha.getHours() && cita.fecha.getMinutes() == nuevaFecha.getMinutes())
+      console.log(cita.barbero == slcBarberosAgenda.value)  
+      return ((cita.barbero == slcBarberosAgenda.value || cita.cliente.email == email.value) && cita.fecha.getFullYear() == nuevaFecha.getFullYear() && cita.fecha.getMonth() == nuevaFecha.getMonth() && cita.fecha.getDate() == nuevaFecha.getDate() && cita.fecha.getHours() == nuevaFecha.getHours() && cita.fecha.getMinutes() == nuevaFecha.getMinutes())
 
     })
 
       if (existe) {
         alert('¡Esta fecha y hora ya está agendada!');
       } else {
+
+        if(servicio.value == 'Corte de pelo'){
+
+        const nuevoAgenda1 = new Agenda(nuevaFecha, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+        
+        const nuevaFecha2 = new Date(nuevaFecha.getTime());
+        nuevaFecha2.setMinutes(nuevaFecha2.getMinutes() + 15);
+
+        const nuevoAgenda2 = new Agenda(nuevaFecha2, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+        agendas.push(nuevoAgenda1)
+        agendas.push(nuevoAgenda2)
+
+        }else if(servicio.value == 'Perfilado y afeitado de barba'){
+          const nuevoAgenda1 = new Agenda(nuevaFecha, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+          agendas.push(nuevoAgenda1)
+        }else if(servicio.value == 'Coloración capilar'){
+
+          const nuevoAgenda1 = new Agenda(nuevaFecha, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+        
+          const nuevaFecha2 = new Date(nuevaFecha.getTime());
+          nuevaFecha2.setMinutes(nuevaFecha2.getMinutes() + 15);
+          const nuevoAgenda2 = new Agenda(nuevaFecha2, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+
+          const nuevaFecha3 = new Date(nuevaFecha2.getTime());
+          nuevaFecha3.setMinutes(nuevaFecha3.getMinutes() + 15);
+          const nuevoAgenda3 = new Agenda(nuevaFecha3, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+
+          const nuevaFecha4 = new Date(nuevaFecha3.getTime());
+          nuevaFecha4.setMinutes(nuevaFecha4.getMinutes() + 15);
+          const nuevoAgenda4 = new Agenda(nuevaFecha4, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+
+          const nuevaFecha5 = new Date(nuevaFecha4.getTime());
+          nuevaFecha5.setMinutes(nuevaFecha5.getMinutes() + 15);
+          const nuevoAgenda5 = new Agenda(nuevaFecha5, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+
+          agendas.push(nuevoAgenda1)
+          agendas.push(nuevoAgenda2)
+          agendas.push(nuevoAgenda3)
+          agendas.push(nuevoAgenda4)
+          agendas.push(nuevoAgenda5)
+        }else if(servicio.value == 'Manicura masculina'){
+          const nuevoAgenda1 = new Agenda(nuevaFecha, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+        
+          const nuevaFecha2 = new Date(nuevaFecha.getTime());
+          nuevaFecha2.setMinutes(nuevaFecha2.getMinutes() + 15);
+          const nuevoAgenda2 = new Agenda(nuevaFecha2, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+
+          const nuevaFecha3 = new Date(nuevaFecha2.getTime());
+          nuevaFecha3.setMinutes(nuevaFecha3.getMinutes() + 15);
+          const nuevoAgenda3 = new Agenda(nuevaFecha3, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+
+          agendas.push(nuevoAgenda1)
+          agendas.push(nuevoAgenda2)
+          agendas.push(nuevoAgenda3)
+        }else if(servicio.value == 'Pack Renovate (Corte + Barba)'){
+          const nuevoAgenda1 = new Agenda(nuevaFecha, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+        
+          const nuevaFecha2 = new Date(nuevaFecha.getTime());
+          nuevaFecha2.setMinutes(nuevaFecha2.getMinutes() + 15);
+          const nuevoAgenda2 = new Agenda(nuevaFecha2, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+
+          const nuevaFecha3 = new Date(nuevaFecha2.getTime());
+          nuevaFecha3.setMinutes(nuevaFecha3.getMinutes() + 15);
+          const nuevoAgenda3 = new Agenda(nuevaFecha3, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+
+          agendas.push(nuevoAgenda1)
+          agendas.push(nuevoAgenda2)
+          agendas.push(nuevoAgenda3)
+        }else if(servicio.value == 'Pack Imagen Completa (Corte + Barba + Manicura)'){
+          
+          const nuevoAgenda1 = new Agenda(nuevaFecha, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+        
+          const nuevaFecha2 = new Date(nuevaFecha.getTime());
+          nuevaFecha2.setMinutes(nuevaFecha2.getMinutes() + 15);
+          const nuevoAgenda2 = new Agenda(nuevaFecha2, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+
+          const nuevaFecha3 = new Date(nuevaFecha2.getTime());
+          nuevaFecha3.setMinutes(nuevaFecha3.getMinutes() + 15);
+          const nuevoAgenda3 = new Agenda(nuevaFecha3, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+
+          const nuevaFecha4 = new Date(nuevaFecha3.getTime());
+          nuevaFecha4.setMinutes(nuevaFecha4.getMinutes() + 15);
+          const nuevoAgenda4 = new Agenda(nuevaFecha4, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+
+          const nuevaFecha5 = new Date(nuevaFecha4.getTime());
+          nuevaFecha5.setMinutes(nuevaFecha5.getMinutes() + 15);
+          const nuevoAgenda5 = new Agenda(nuevaFecha5, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+
+          const nuevaFecha6 = new Date(nuevaFecha5.getTime());
+          nuevaFecha6.setMinutes(nuevaFecha6.getMinutes() + 15);
+          const nuevoAgenda6 = new Agenda(nuevaFecha6, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+
+
+          agendas.push(nuevoAgenda1)
+          agendas.push(nuevoAgenda2)
+          agendas.push(nuevoAgenda3)
+          agendas.push(nuevoAgenda4)
+          agendas.push(nuevoAgenda5)
+          agendas.push(nuevoAgenda6)
+
+        }else if(servicio.value == 'Color + Corte' ){
+          const nuevoAgenda1 = new Agenda(nuevaFecha, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+        
+          const nuevaFecha2 = new Date(nuevaFecha.getTime());
+          nuevaFecha2.setMinutes(nuevaFecha2.getMinutes() + 15);
+          const nuevoAgenda2 = new Agenda(nuevaFecha2, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+
+          const nuevaFecha3 = new Date(nuevaFecha2.getTime());
+          nuevaFecha3.setMinutes(nuevaFecha3.getMinutes() + 15);
+          const nuevoAgenda3 = new Agenda(nuevaFecha3, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+
+          const nuevaFecha4 = new Date(nuevaFecha3.getTime());
+          nuevaFecha4.setMinutes(nuevaFecha4.getMinutes() + 15);
+          const nuevoAgenda4 = new Agenda(nuevaFecha4, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+
+          const nuevaFecha5 = new Date(nuevaFecha4.getTime());
+          nuevaFecha5.setMinutes(nuevaFecha5.getMinutes() + 15);
+          const nuevoAgenda5 = new Agenda(nuevaFecha5, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+
+          const nuevaFecha6 = new Date(nuevaFecha5.getTime());
+          nuevaFecha6.setMinutes(nuevaFecha6.getMinutes() + 15);
+          const nuevoAgenda6 = new Agenda(nuevaFecha6, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+
+          const nuevaFecha7 = new Date(nuevaFecha6.getTime());
+          nuevaFecha7.setMinutes(nuevaFecha7.getMinutes() + 15);
+          const nuevoAgenda7 = new Agenda(nuevaFecha7, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
+
+
+          agendas.push(nuevoAgenda1)
+          agendas.push(nuevoAgenda2)
+          agendas.push(nuevoAgenda3)
+          agendas.push(nuevoAgenda4)
+          agendas.push(nuevoAgenda5)
+          agendas.push(nuevoAgenda6)
+          agendas.push(nuevoAgenda7)
+        }
         alert('Agendado');
-        const nuevoAgenda = new Agenda(nuevaFecha, servicio.value, nombre.value, email.value, slcBarberosAgenda.value )
-        agendas.push(nuevoAgenda)
       }
     }
