afa7a8654f2e5604467b34de317ea1f688cf4720
FACULTADES\alumnoFI <a>
2025-08-04 10:08:34 -0300

    segundo test agendar

--- __test__/agendar.test.js
+++ __test__/agendar.test.js
@@ -1,13 +1,30 @@
 const agendarCita = require("../js/CU/Agendar")
 
-test("Test de agendar cita", function(){
-    const nombre = "Gaston";
-    const email ="gassjau@gmail.com";
-    const servicio = "Corte de pelo";
-    const slcBarberosAgenda = "Ana Gómez";
-    const fecha = "2025-07-29";
-    const hora = "11:15";
 
-    const resultado = agendarCita(nombre, email, servicio, slcBarberosAgenda, fecha, hora);
-    expect(resultado).toBe('OK')
+describe("CU: Agendar", () => {
+
+
+    test("Test de agendar cita", function(){
+        const nombre = "Gaston";
+        const email ="gassjau@gmail.com";
+        const servicio = "Corte de pelo";
+        const slcBarberosAgenda = "Ana Gómez";
+        const fecha = "2025-07-29";
+        const hora = "11:15";
+    
+        const resultado = agendarCita(nombre, email, servicio, slcBarberosAgenda, fecha, hora);
+        expect(resultado).toBe('OK')
+    })
+    
+    test("Test de agendar cita error por falta de datos", function(){
+        const nombre = "Gaston";
+        const email ="gassjau@gmail.com";
+        const servicio = "Corte de pelo";
+        const slcBarberosAgenda = "Ana Gómez";
+        const fecha = "";
+        const hora = "";
+    
+        const resultado = agendarCita(nombre, email, servicio, slcBarberosAgenda, fecha, hora);
+        expect(resultado).toBe('Campos invalidos')
+    })
 })
